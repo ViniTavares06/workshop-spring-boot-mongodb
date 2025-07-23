cc2a9604b9657738f65f304533bd4d074f4b275f
Vinicius Tavares <vinixz06@gmail.com>
2025-07-23 18:29:27 -0300

    Obtendo post por id

--- src/main/java/com/workshop/workshopmongo/resources/UserResource.java
+++ src/main/java/com/workshop/workshopmongo/resources/UserResource.java
@@ -28,9 +28,9 @@ public class UserResource {
     }
 
     @RequestMapping(value="/{id}", method=RequestMethod.GET)
-    public ResponseEntity<User> findById(@PathVariable String id) {
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 
     @RequestMapping(method=RequestMethod.POST)
