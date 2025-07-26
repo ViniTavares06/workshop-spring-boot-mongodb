81173061b95deffbdc22fc7d696743d6d75c4f9c
Vinicius Tavares <vinixz06@gmail.com>
2025-07-26 18:40:17 -0300

    Consulta simples com query methods

--- src/main/java/com/workshop/workshopmongo/resources/PostResource.java
+++ src/main/java/com/workshop/workshopmongo/resources/PostResource.java
@@ -1,10 +1,15 @@
 package com.workshop.workshopmongo.resources;
 
 import com.workshop.workshopmongo.domain.Post;
+import com.workshop.workshopmongo.resources.util.URL;
 import com.workshop.workshopmongo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -17,6 +22,18 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body((obj));
+    }
+
+    public ResponseEntity<List<Post>> findByTitle(
+            @RequestParam(value = "text", defaultValue = "") String text
+    ) {
+        try {
+            String decodedText = URL.decodeParam(text);
+            List<Post> list = service.findByTitle(decodedText);
+            return ResponseEntity.ok().body(list);
+        } catch (UnsupportedEncodingException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
 
